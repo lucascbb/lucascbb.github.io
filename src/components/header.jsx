69625ff948982efab0deafaa43c3412ae24c939a
lucascbb <lucasbaroquello@hotmail.com>
2023-03-03 12:33:39 -0300

    1

---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+function Header() {
+
+  return (
+    <div>
+      <p>Header</p>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/pages/home.jsx
@@ -0,0 +1,16 @@
+import Header from '../components/header';
+
+function Counter() {
+
+  return (
+    <div>
+      <Header />
+      <h1>Introducao sobre mim</h1>
+      <h1>Projetos</h1>
+      <h1>Contato</h1>
+      <h1>Habilidades</h1>
+    </div>
+  );
+}
+
+export default Counter;
