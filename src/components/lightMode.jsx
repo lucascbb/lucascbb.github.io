736ecefbb3e19217b3844d0a47b28c5b1452846b
Lucas <lucasbaroquello@hotmail.com>
2023-04-02 17:40:17 -0300

    btn dark/light

--- src/components/lightMode.jsx
+++ src/components/lightMode.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import '../styles/lightMode.css';
 import ThemeContext from '../context/ThemeContext';
+import { CiLight, CiDark } from 'react-icons/ci'
 
 function LightMode() {
     const { light, setLight } = useContext(ThemeContext)
@@ -14,7 +15,7 @@ function LightMode() {
     <div>
         <label className="switch">
             <input type="checkbox" checked={ light } onChange={ funcLight } />
-            <span className="slider round"></span>
+            <span className="slider round">{light ? <CiLight className='day'/> : <CiDark className='night'/> }</span>
         </label>
     </div>
   );
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -8,26 +8,38 @@ import gifContato from '../images/sent.gif'
 import '../styles/contact.css'
 
 function Home() {
-  const { light } = useContext(ThemeContext)
+    const { light } = useContext(ThemeContext)
+    const handleClick = () => {
+        navigator.clipboard.writeText('lucasbaroquello@hotmail.com');
+        document.getElementById("copyEmail").innerHTML = "Email copied!";
+        document.getElementById("copyEmail").style.backgroundColor = "white"
+        setTimeout(function() {
+            document.getElementById("copyEmail").innerHTML = ''
+            document.getElementById("copyEmail").style.backgroundColor = "transparent"
+          }, 3000);
+    }
+
   return (
     <div className='contactPai'>
+        
         <div className='contactLeft'>
-            <h1 className='titleContact'>Contact</h1>
-            <div className='linkContact'>
+            <h1 className='titleContact'>Contato</h1>
+            <div className='emailContact'>
                 <h1 className='iconContact'><MdEmail className='iconCo'/>lucasbaroquello@hotmail.com</h1>
-                <MdCopyAll />
+                <button onClick={ handleClick } className='btnCopyEmail'><MdCopyAll className='iconCopyEmail'/></button>
+                <p id='copyEmail'></p>
             </div>
             <a  className='linkContact' href='https://api.whatsapp.com/send?phone=5511941139277&text=Ol%C3%A1,%20tudo%20bem?%20Encontrei%20seu%20contato%20atrav%C3%A9s%20do%20Github.%20Eu%20sou%20o/a%20(insira%20seu%20nome%20aqui'>
                 <h1 className='iconContact'><SiWhatsapp className='iconCo'/>Whatsapp</h1>
-                <FiExternalLink/>
+                <FiExternalLink className='iconLink'/>
             </a>
             <a className='linkContact' href='https://github.com/lucascbb'>
                 <h1 className='iconContact'><SiGithub className='iconCo'/>Github</h1>
-                <FiExternalLink/>
+                <FiExternalLink className='iconLink'/>
             </a>
             <a className='linkContact' href='https://www.linkedin.com/in/lucas-baroquello/'>
                 <h1 className='iconContact'><SiLinkedin className='iconCo'/>Linkedin</h1>
-                <FiExternalLink/>
+                <FiExternalLink className='iconLink'/>
             </a>
         </div>
         <div className='contactRight'>
