985950c2c07f5e22ac3d723765825a8ed84044db
Lucas <lucasbaroquello@hotmail.com>
2023-05-27 03:01:45 -0300

    Page Home css light/dark mode

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,6 @@
 import Memoji from '../components/memoji';
 // import NavBar from '../components/navBar';
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import ThemeContext from '../context/ThemeContext';
 import '../styles/home.css';
 import imgHome1light from '../images/homePage-light1.jpg'
@@ -11,38 +11,38 @@ import imgHome2dark from '../images/homePage-dark2.png'
 function Home() {
   const { light } = useContext(ThemeContext);
   const [nameProfile, setNameProfile] = useState("DESIGNER")
-  const [classe, setclasse] = useState("name3lightD")
+  const [classe, setclasse] = useState("name3D")
 
   const minhafuncao = () => {
     if (nameProfile === "DESIGNER") {
-      setclasse("name3lightF")
+      setclasse("name3F")
       setNameProfile("FRONT END")
     } else if (nameProfile === "FRONT END") {
-      setclasse("name3lightB")
+      setclasse("name3B")
       setNameProfile("BACK END")
     } else {
-      setclasse("name3lightD")
+      setclasse("name3D")
       setNameProfile("DESIGNER")
     }
   }
 
-  setTimeout(minhafuncao, 2000)
+  setTimeout(minhafuncao, 4000)
 
   return (
     <div className="paidivBar">
       <Memoji />
+      {/* <iframe src='https://my.spline.design/iphone14procopy-b81036445248c0db18611c43c726f197/' frameborder='0' width='100%' height='100%'></iframe> */}
       <img src={ light ? imgHome2dark : imgHome2light } alt=''className="imgdivBar2" />
       <img src={ light ? imgHome1dark : imgHome1light } alt=''className="imgdivBar" />
       <div className="divBar">
         <div className="divBarName">
-          <h1 className={ light ? "name1light" : "name1dark" } >HI,I`M LUCAS,</h1>
+          <h1 className={ light ? "name1light" : "name1dark" } >HI, I`M LUCAS</h1>
           <h1 className={ light ? "name2light" : "name2dark" }>BAROQUELLO</h1>
-          {/* <h1 className={ classe }>{ nameProfile }</h1> */}
-          <h1 className={ light ? "name3light" : "name3dark" }>{ nameProfile }</h1>
+          <h1 className={ classe } >{ nameProfile }</h1>
           <div className='fundo' />
         </div>
         <div className="divBarPara">
-          <p>
+          <p className="para-home">
             +1,5 ANO DE EXPERIENCIA EM DESENVOLVIMENTO WEB <br/>
             +20 PROJETOS DESENVOLVIDOS (FRONT + BACK + FULL STACK) <br/>
             +3 ANOS ATUANDO COMO DESIGNER E MARKETING
