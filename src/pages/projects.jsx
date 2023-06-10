5e56e812946a10f27896d45f3d3f7ee1bdff7e20
Lucas <lucasbaroquello@hotmail.com>
2023-06-10 00:34:22 -0300

    css card 2

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 import ThemeContext from '../context/ThemeContext';
 import { useContext } from 'react';
 import '../styles/projects.css'
-import image6 from '../images/imgCard-6.png'
-import image7 from '../images/trybetunes-Mockup.png'
+import image3 from '../images/trybetunes-Mockup3.png'
+import image2 from '../images/trybetunes-Mockup2.png'
+import image1 from '../images/trybetunes-Mockup.png'
+import recipes1 from '../images/recipes-Mockup.png'
+import recipes2 from '../images/recipes-Mockup2.png'
 import gif1 from '../images/laptop-mockup.png'
 import gif2 from '../images/iphone-mockup.png'
 import gif3 from '../images/Trybetunes.png'
@@ -11,6 +14,7 @@ import gi14 from '../images/laptop-mockup2.png'
 import html from '../images/html.png'
 import css from '../images/css.png'
 import javascript from '../images/javascript.png'
+import react from '../images/react.png'
 import { IoIosArrowDroprightCircle } from "react-icons/io"
 import { IoIosArrowDropleftCircle } from "react-icons/io"
 import { TbCircle } from "react-icons/tb"
@@ -22,6 +26,7 @@ import imgHomeDark from '../images/homePage-Dark.png'
 function Projects() {
   const { light } = useContext(ThemeContext);
   const [carousel, setCarousel] = useState(1);
+  const [active, setActive] = useState(7);
   const [cards, setCards] = useState([
     {
       title: "Trybetunes",
@@ -33,109 +38,111 @@ function Projects() {
         { img: html, name: "React" },
       ],
       // backgroundImage: `${image7}`,
-      img1: gif1,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "Trivia",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      // backgroundImage: `${image7}`,
-      img1: gif1,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "Trybe Futebol",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      // backgroundImage: `${image7}`,
-      img1: gif1,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "App de Receitas",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      // backgroundImage: `${image7}`,
-      img1: gif1,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "Trybetunes",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      // backgroundImage: `${image7}`,
-      img1: gif1,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "Trivia",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      // backgroundImage: `${image7}`,
-      img1: gif1,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "Trybe Futebol",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      // backgroundImage: `${image6}`,
-      img1: gi14,
-      img2: gif2,
-      img3: gif3,
-    },
-    {
-      title: "Trybetunes",
-      subtitle: "Um aplicativo de receitas de bebidas e comidas",
-      stacks: [
-        { img: html, name: "HTML" },
-        { img: css, name: "CSS" },
-        { img: javascript, name: "JavaScript" },
-        { img: html, name: "React" },
-      ],
-      linkProject: "www.google.com",
-      linkCode: "www.google.com",
-      backgroundImage: `${image7}`,
       // img1: gif1,
       // img2: gif2,
       // img3: gif3,
+    },
+    {
+      title: "Trivia",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: html, name: "React" },
+      ],
+      // backgroundImage: `${image7}`,
+      // img1: gif1,
+      // img2: gif2,
+      // img3: gif3,
+    },
+    {
+      title: "Trybe Futebol",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: html, name: "React" },
+      ],
+      // backgroundImage: `${image7}`,
+      // img1: gif1,
+      // img2: gif2,
+      // img3: gif3,
+    },
+    {
+      title: "Receitas",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: html, name: "React" },
+      ],
+      // backgroundImage: `${image7}`,
+      // img1: gif1,
+      // img2: gif2,
+      // img3: gif3,
+    },
+    {
+      title: "Trybetunes",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: html, name: "React" },
+      ],
+      // backgroundImage: `${image7}`,
+      // img1: gif1,
+      // img2: gif2,
+      // img3: gif3,
+    },
+    {
+      title: "Trivia",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: html, name: "React" },
+      ],
+      // backgroundImage: `${image7}`,
+      // img1: gif1,
+      // img2: gif2,
+      // img3: gif3,
+    },
+    {
+      title: "Recipes App",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: react, name: "React" },
+      ],
+      linkProject: "https://github.com/lucascbb/trybetunes",
+      linkCode: "https://lucascbb.github.io/trybetunes/",
+      color: "#933b23",
+      backgroundImage: `${recipes1}`,
+      backgroundImage2: `${recipes2}`,
+      backgroundImage3: `${image3}`,
+    },
+    {
+      title: "Trybetunes",
+      subtitle: "Um aplicativo de receitas de bebidas e comidas",
+      stacks: [
+        { img: html, name: "HTML" },
+        { img: css, name: "CSS" },
+        { img: javascript, name: "JavaScript" },
+        { img: react, name: "React" },
+      ],
+      linkProject: "https://github.com/lucascbb/trybetunes",
+      linkCode: "https://lucascbb.github.io/trybetunes/",
+      color: "rgb(184, 118, 19)",
+      backgroundImage: `${image1}`,
+      backgroundImage2: `${image2}`,
+      backgroundImage3: `${image3}`,
     },
   ])
 
@@ -144,7 +151,7 @@ function Projects() {
     document.getElementById(`titleCard-${cards.length - 1}`).className = "titleCard-selected"
     document.getElementById(`subtitleCard-${cards.length - 1}`).className = "subtitleCard-selected"
     document.getElementById(`stacksCard-${cards.length - 1}`).className = "stacksCard-selected"
-    document.getElementById(`imgCard-${cards.length - 1}`).className = "imgCard-selected"
+    // document.getElementById(`imgCard-${cards.length - 1}`).className = "imgCard-selected"
     document.getElementById(`paiBtnsCard-${cards.length - 1}`).className = "paiBtnsCard-selected"
     document.getElementById(`paiCirclesCard-${cards.length - 1}`).className = "paiCirclesCard-selected"
     document.getElementById(`linkProjectPai-${cards.length - 1}`).className = "linkProjectPai-selected"
@@ -157,7 +164,7 @@ function Projects() {
       document.getElementsByClassName("titleCard-selected")[0].className = "titleCard"
       document.getElementsByClassName("subtitleCard-selected")[0].className = "subtitleCard"
       document.getElementsByClassName("stacksCard-selected")[0].className = "stacksCard"
-      document.getElementsByClassName("imgCard-selected")[0].className = "imgCard"
+      // document.getElementsByClassName("imgCard-selected")[0].className = "imgCard"
       document.getElementsByClassName("paiBtnsCard-selected")[0].className = "paiBtnsCard"
       document.getElementsByClassName("paiCirclesCard-selected")[0].className = "paiCirclesCard"
       document.getElementsByClassName("linkProjectPai-selected")[0].className = "linkProjectPai"
@@ -165,12 +172,13 @@ function Projects() {
     }
 
     if (classe === "paiCard") { setCarousel(1) }
+    setActive(id)
 
     document.getElementById(`divCard-${id}`).className = "paiCard-selected"
     document.getElementById(`titleCard-${id}`).className = "titleCard-selected"
     document.getElementById(`subtitleCard-${id}`).className = "subtitleCard-selected"
     document.getElementById(`stacksCard-${id}`).className = "stacksCard-selected"
-    document.getElementById(`imgCard-${id}`).className = "imgCard-selected"
+    // document.getElementById(`imgCard-${id}`).className = "imgCard-selected"
     document.getElementById(`paiBtnsCard-${id}`).className = "paiBtnsCard-selected"
     document.getElementById(`paiCirclesCard-${id}`).className = "paiCirclesCard-selected"
     document.getElementById(`linkProjectPai-${id}`).className = "linkProjectPai-selected"
@@ -200,15 +208,20 @@ function Projects() {
               className='paiCard'
               onClick={(e) => changeCard(i, e.currentTarget.className)}
               id={`divCard-${i}`} key={i}
-              style={{ 
-                backgroundImage: `linear-gradient(0deg, rgba(0,0,0,1) 15%, rgba(77,100,134,0) 100%), url(${ele.backgroundImage})`,
+              style={{
+                backgroundImage: carousel === 1 && i === active ? `url(${ele.backgroundImage})` : carousel === 2 && i === active ? `url(${ele.backgroundImage2})` : carousel === 3 && i === active ? `url(${ele.backgroundImage3})` : `url(${ele.backgroundImage})`,
                 backgroundPosition: 'center',
-                backgroundSize: '100%',
-                backgroundRepeat: 'no-repeat'
+                backgroundRepeat: 'no-repeat',
+                // filter: 'blur(2px)',
+                // filter: carousel === 1 && i === active ? `blur(0)` : carousel === 2 && i === active ? `blur(0)` : carousel === 3 && i === active ? `blur(0)` : 'blur(1px)',
               }}
             >
               <h1 className='titleCard' id={`titleCard-${i}`}>{ele.title}</h1>
-              <div className='stacksCard' id={`stacksCard-${i}`}>
+              <div className='paiBtnsCard' id={`paiBtnsCard-${i}`}>
+                <button className='leftBtnCard' id={`leftBtnCard-${i}`} onClick={handleLeft}><IoIosArrowDropleftCircle /></button>
+                <button className='rightBtnCard' id={`rightBtnCard-${i}`} onClick={handleRight}><IoIosArrowDroprightCircle /></button>
+              </div>
+              <div className='stacksCard' id={`stacksCard-${i}`} style={carousel !== 1 ? { opacity: '0' } : null}>
                 {ele.stacks.map((stack, index) => (
                   <div key={index} className='stacksCardteste'>
                     <img src={stack.img} alt={`Stack ${index + 1}`} width="80px" />
@@ -216,28 +229,24 @@ function Projects() {
                   </div>
                 ))}
               </div>
-              <p className='subtitleCard' id={`subtitleCard-${i}`}>{ele.subtitle}</p>
-              {carousel === 1 ? (
+              <p className='subtitleCard' id={`subtitleCard-${i}`} style={carousel !== 1 ? { opacity: '0' } : null}>{ele.subtitle}</p>
+              {/* {carousel === 1 ? (
                 <img src={ele.img1} alt="Imagem 1" className='imgCard' id={`imgCard-${i}`} />
               ) : carousel === 2 ? (
                 <img src={ele.img2} alt="Imagem 2" className='imgCard' id={`imgCard-${i}`} />
               ) : (
                 <img src={ele.img3} alt="Imagem 3" className='imgCard' id={`imgCard-${i}`} />
-              )}
-              <div className='paiBtnsCard' id={`paiBtnsCard-${i}`}>
-                <button className='leftBtnCard' id={`leftBtnCard-${i}`} onClick={handleLeft}><IoIosArrowDropleftCircle /></button>
-                <button className='rightBtnCard' id={`rightBtnCard-${i}`} onClick={handleRight}><IoIosArrowDroprightCircle /></button>
-              </div>
+              )} */}
               <div className='linkProjectPai' id={`linkProjectPai-${i}`}>
-                <a href="https://www.w3schools.com/" target="_blank" rel="noreferrer" className='btnLink'>
+                <a href={ele.linkCode} target="_blank" rel="noreferrer" className='btnLink' style={{ backgroundColor: ele.color }}>
                   <div className='linkProject'>
                     <p>Ver Projeto</p>
                     <GoLinkExternal />
                   </div>
                 </a>
-                <a href="https://www.w3schools.com/" target="_blank" rel="noreferrer" className='btnLink'>
+                <a href={ele.linkProject} target="_blank" rel="noreferrer" className='btnLink' style={{ backgroundColor: ele.color }}>
                   <div className='linkProject'>
-                    <p>Ver Code</p>
+                    <p>Ver Código</p>
                     <GoLinkExternal />
                   </div>
                 </a>
